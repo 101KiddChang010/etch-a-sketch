b0725042f1edf4c4f4b18ae0ba860f3d679d158d
Kidd <kiddochang02@gmail.com>
2022-09-09 02:14:39 -0400

    Added functional drawing by hovering over grid

--- scripts.js
+++ scripts.js
@@ -1,9 +1,22 @@
 
 const getContainerDiv = document.getElementById("container");
+let OneIncrementedByOne = 1;
 
 
 for (let x = 0; x < 256; x++) {
+    let gridID = "draw";
+    gridID += OneIncrementedByOne.toString();
     const createDiv = document.createElement("div");
+    
+    createDiv.setAttribute("id",gridID);
     createDiv.classList.add("draw");
+
     getContainerDiv.appendChild(createDiv);
+
+    document.getElementById(gridID).addEventListener("mouseout", function() {
+        document.getElementById(gridID).style.backgroundColor = "black";
+    })
+
+    OneIncrementedByOne++;
 }
+
